0c5499d55ac3bd7416dcb8d97089c752cb4ad1f7
nisjety <ima_stovers24@hotmail.com>
2023-12-08 10:57:37 +0100

    first commit

---
+++ src/main/java/com/example/maschinefactory/MaschineFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.maschinefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaschineFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaschineFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maschinefactory/MaschineFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maschinefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaschineFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
